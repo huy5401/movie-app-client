6ea96916da430ba7c10e2d2fc0e522ed27e90565
QuangHuy5401 <nghhuy5401@gmail.com>
2023-02-03 23:33:23 +0700

    header search

--- src/components/Layout/DefaultLayout/Header/Header.js
+++ src/components/Layout/DefaultLayout/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind'
+import images from '../../../../assets/images'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 
 const cx = classNames.bind(styles)
 
@@ -9,8 +12,20 @@ export default function Header() {
     <header className={cx('wrapper')}> 
         <div className={cx('top')}>
             <div className={cx('top-container')}>
-              
+                <img className={cx('logo')} src={images.logo} alt='logo'></img>
+                <div className={cx('search')}>
+                  <input type='text' placeholder='Search movies or actor'></input>
+                  <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />                  
+                  </button>
+                </div>
+                <div className={cx('action')}>
+
+                </div>
             </div>
+        </div>
+        <div className={cx('bottom')}>
+            <div className={cx('bot-container')}></div>
         </div>
     </header>
   )
